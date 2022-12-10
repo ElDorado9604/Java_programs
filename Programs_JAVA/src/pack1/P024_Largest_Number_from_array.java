536fbc3253e7d9a7a160783effc8b45526508f7e
HP <HP@LAPTOP-9FGT47P4>
2022-12-10 18:09:29 +0530

    Array changed

--- Programs_JAVA/src/pack1/P024_Largest_Number_from_array.java
+++ Programs_JAVA/src/pack1/P024_Largest_Number_from_array.java
@@ -5,7 +5,7 @@ public class P024_Largest_Number_from_array {
 	public static void main(String[] args) {
 		//Largest Number from array
 		
-		int a [] = {23,48,56,43,57,52,2,81,17,33,44};
+		int a [] = {23,48,56,43,57,52,2,81,17,33,44,78};
 		
 		for(int i = 0 ; i < a.length-1 ; i++)
 		{
